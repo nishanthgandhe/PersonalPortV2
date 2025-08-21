0af8a928744ee03feb5814eccc89bb5243b6a6f5
Nishanth Gandhe <nishanth.gandhe05@tamu.edu>
2025-08-21 04:11:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/PersonalPortV2/', // Set the base path for GitHub Pages
+  base: '/', // Root path for custom domain
 })
